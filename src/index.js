1afee8a6b309b6e77d0414143603f9359a1e5e70
Mateusz Drulis <mateusz.drulis@gmail.com>
2020-02-10 14:19:39 +0000

    Adds userId to url
    Enables debugging different users using url query

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react"
+import queryString from "qs"
 import { render } from "react-dom"
 import { CanduProvider } from "@candulabs/react-sdk"
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
-import Ant from "./apps/Ant"
+import { Ant } from "./apps"
 import antStyleguide from "./styles/antStyleguide"
 
 import { initPendo } from "./clients/pendo"
@@ -11,22 +12,31 @@ import { initPendo } from "./clients/pendo"
 // initialize pendo with fake user
 initPendo()
 
-class App extends React.Component {
+class Steamery extends React.Component {
   render() {
     const selectedApp = <Ant />
     const selectedStyleguide = antStyleguide
     return (
       <Router>
-        <CanduProvider
-          clientToken="dR8ZTszcnp"
-          userId="1"
-          styleguide={selectedStyleguide}
-        >
-          {selectedApp}
-        </CanduProvider>
+        <Route path="*">
+          {props => {
+            const { userId = 1 } = queryString.parse(
+              props.location.search.replace("?", "")
+            )
+            return (
+              <CanduProvider
+                clientToken="dR8ZTszcnp"
+                userId={userId}
+                styleguide={selectedStyleguide}
+              >
+                {selectedApp}
+              </CanduProvider>
+            )
+          }}
+        </Route>
       </Router>
     )
   }
 }
 
-render(<App />, document.getElementById("root"))
+render(<Steamery />, document.getElementById("root"))
